8dab82e2e1ca671b5128f6e8881dc7184cc5c86c
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-11 19:25:34 -0500

    add redux Provider and car reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Redux from 'react-redux';
+
+import car from 'reducers/car';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const carStore = Redux.createStore (car.reducer);
+
+ReactDOM.render (
+  <Redux.Provider store={carStore}>
+    <App/>
+  </Redux.Provider>,
+  document.getElementById ('root')
+);
