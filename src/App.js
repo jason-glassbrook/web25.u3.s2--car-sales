66249f5b7960cf49e9cef14347ef2a46848e9863
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-11 20:00:20 -0500

    rename: addFeature <- addItem

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = () => {
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
+  const addFeature = item => {
+    // dipsatch an action here to add an item
   };
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
+  const removeFeature = item => {
+    // dispatch an action here to remove an item
   };
 
   return (
