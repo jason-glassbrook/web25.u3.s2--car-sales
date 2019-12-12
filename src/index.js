2e46932409ca554354602bea3aa3596342e0485b
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-12 17:10:36 -0500

    move car store into state/car

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as ReactRedux from 'react-redux';
-import * as redux from 'redux';
 
-import car from 'reducers/car';
+import car from 'states/car';
 
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const carStore = redux.createStore (car.reducer);
-
 ReactDOM.render (
-  <ReactRedux.Provider store={carStore}>
+  <ReactRedux.Provider store={car.store}>
     <App/>
   </ReactRedux.Provider>,
   document.getElementById ('root')
