24f0e88190394da0005eef3d2b39177226455f65
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 10:41:06 -0500

    remove redux from App -- don't need

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import React from 'react';
-import * as ReactRedux from 'react-redux';
-
-import car from 'states/car';
 
 import Header from 'components/Header';
 import AddedFeatures from 'components/AddedFeatures';
 import AdditionalFeatures from 'components/AdditionalFeatures';
 import Total from 'components/Total';
 
-const App = () => {
-  const [ state, dispatch ] = React.useReducer (car.reducer, car.init);
-  // const send = car.actions.makers (dispatch);
-
-  // const addFeature = (item) => send.ADD_FEATURE (item);
-  // const removeFeature = (item) => send.REMOVE_FEATURE (item);
-
-  return (
-    <div className="boxes">
-      <div className="box">
-        <Header/>
-        <AddedFeatures/>
-      </div>
-      <div className="box">
-        <AdditionalFeatures/>
-        <Total/>
-      </div>
+const App = () => (
+  <div className="boxes">
+    <div className="box">
+      <Header/>
+      <AddedFeatures/>
     </div>
-  );
-};
-
-const mapStateToProps = (state) => ({
-});
-
-export default (
-  ReactRedux.connect (
-    mapStateToProps, {}
-  ) (
-    App
-  )
+    <div className="box">
+      <AdditionalFeatures/>
+      <Total/>
+    </div>
+  </div>
 );
+
+export default App;
