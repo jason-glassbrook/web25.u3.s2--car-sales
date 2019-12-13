2c21427708d0cd1643218ee3aaebf7ce00f283de
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-13 10:49:30 -0500

    add onClick events to add/remove features

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const AddedFeature = (props) => {
+const AddedFeature = ({ feature, removeFeature, props }) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button
+      className="button"
+      onClick={() => removeFeature (feature)}
+      >X</button>
+      {feature.name}
     </li>
   );
 };
